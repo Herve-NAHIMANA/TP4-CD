91e7940f07235eb5751690631dfec1ae086726f3
Herve NAHIMANA <herve.nahimana1996@gmail.com>
2023-10-20 00:13:35 +0200

    first commit

--- deployment.sh
+++ deployment.sh
@@ -25,7 +25,7 @@ if ! command -v ansible &> /dev/null; then
      apt update
      apt install -y python3-pip
      apt install -y ansible
-     pip3 install -y google-auth
+     yes | pip3 install google-auth
 fi
 ./google-cloud-sdk/bin/gcloud services enable compute.googleapis.com --project=$PROJET
 ./google-cloud-sdk/bin/gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJET
