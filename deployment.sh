b760a041fb8286d7c4d77eedbe1c88f18622ad7a
Herve NAHIMANA <herve.nahimana1996@gmail.com>
2023-10-19 17:04:41 +0200

    update jenkinsfile

--- deployment.sh
+++ deployment.sh
@@ -13,15 +13,12 @@ ZONE="us-east1-b"
     IFS=$'\n'
 # 1- Vérifier et installer Terraform si nécessaire
 if ! command -v terraform &> /dev/null; then
-    apt-get update &&  apt-get install -y gnupg software-properties-common
-    wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    tee /etc/apt/sources.list.d/hashicorp.list
-    apt update
-    apt-get install terraform
+    apt-get update
+    apt install unzip
+    terraform_url=$(curl -s https://releases.hashicorp.com/terraform/ | grep -o 'https://releases.hashicorp.com/terraform/[0-9.]\+/terraform_[0-9.]\+_linux_amd64.zip' | head -n 1)
+    curl -o terraform.zip $terraform_url
+    unzip terraform.zip
+    mv terraform /usr/local/bin/
 fi
 
 # 2- Vérifier et installer Ansible si nécessaire
@@ -30,9 +27,9 @@ if ! command -v ansible &> /dev/null; then
      apt install -y ansible
 fi
 
-/google-cloud-sdk/bin/gcloud services enable compute.googleapis.com --project=$PROJET
-/google-cloud-sdk/bin/gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJET
-/google-cloud-sdk/bin/gcloud services enable iam.googleapis.com --project=$PROJET
+# /google-cloud-sdk/bin/gcloud services enable compute.googleapis.com --project=$PROJET
+# /google-cloud-sdk/bin/gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJET
+# /google-cloud-sdk/bin/gcloud services enable iam.googleapis.com --project=$PROJET
 # Vérification de la présence des fichiers Terraform et exécution de terraform init
 if [ ! -d "terraform" ]; then
     git clone https://github.com/Herve-NAHIMANA/TP4-CD.git
