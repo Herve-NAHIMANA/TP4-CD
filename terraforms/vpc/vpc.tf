f12ce8e2cc1835bae7cf6e6daed8150acdc17e78
Herve NAHIMANA <herve.nahimana1996@gmail.com>
2023-10-20 16:08:22 +0200

    update vpc.tf

--- terraforms/vpc/vpc.tf
+++ terraforms/vpc/vpc.tf
@@ -5,12 +5,12 @@ resource "google_compute_subnetwork" "Test" {
   network       = google_compute_network.custom-test.id
   secondary_ip_range {
     range_name    = "services"
-    ip_cidr_range = "10.0.1.0/24"
+    ip_cidr_range = "10.2.1.0/24"
   }
 
   secondary_ip_range {
     range_name    = "pods"
-    ip_cidr_range = "10.0.1.16/24"
+    ip_cidr_range = "10.2.1.16/24"
   }
 }
 resource "google_compute_network" "custom-test" {
