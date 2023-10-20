cd116b6e9006ac5e7a623a6a8a66252a70be66db
Herve NAHIMANA <herve.nahimana1996@gmail.com>
2023-10-20 16:18:23 +0200

    update vpc.tf

--- terraforms/kubernetes/kubernetes.tf
+++ terraforms/kubernetes/kubernetes.tf
@@ -1,6 +1,8 @@
 resource "google_container_cluster" "my_cluster" {
   name               = "my-gke-cluster"
   location           = var.gcp_region  # Modifiez la région selon vos besoins
+  network = var.vpc_name
+  subnetwork = var.subnet_name
   project = var.gcp_project
   initial_node_count = 3
 
